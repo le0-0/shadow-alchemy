350ea8b4a9423c469e766043978629f9fe1b3e58
le0-0 <leonard.schoyen@gmail.com>
2024-07-22 21:40:25 +0200

    Player prefab with initial sideways movement.

---
+++ application/Shadow Alchemist/Assets/Prefabs/Player/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System;
+
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+  [SerializeField]
+  private static float MAX_SPEED = 5f;
+
+  [SerializeField]
+  private static float ACCELERATION = 20f;
+
+  [SerializeField]
+  private static float OFF_GROUND_MODIFIER = 0.5f;
+
+  private Rigidbody2D rigidbody2d;
+
+  void Start()
+  {
+    rigidbody2d = GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate()
+  {
+    bool isAcceleratingSideways = false;
+    float xVelocity = rigidbody2d.velocity.x;
+
+    if (Input.GetKey(KeyCode.A))
+    {
+      isAcceleratingSideways = true;
+      xVelocity = MathF.Max(-MAX_SPEED, xVelocity - (ACCELERATION * Time.deltaTime));
+    }
+    if (Input.GetKey(KeyCode.D))
+		{
+			isAcceleratingSideways = true;
+			xVelocity = MathF.Min(MAX_SPEED, xVelocity + (ACCELERATION * Time.deltaTime));
+		}
+
+    if (!isAcceleratingSideways)
+    {
+      xVelocity = MathF.Sign(xVelocity) * MathF.Max(0, MathF.Abs(xVelocity) - (ACCELERATION * Time.deltaTime));
+    }
+
+    rigidbody2d.velocity = new Vector2 (xVelocity, rigidbody2d.velocity.y);
+  }
+}
