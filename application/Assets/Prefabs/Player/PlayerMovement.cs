91e17b643d735780c36f858a863bf67a5e3d53ef
le0-0 <leonard.schoyen@gmail.com>
2024-07-23 16:42:13 +0200

    Refactor

--- application/Assets/Prefabs/Player/PlayerMovement.cs
+++ application/Assets/Prefabs/Player/PlayerMovement.cs
@@ -13,28 +13,55 @@ public class PlayerMovement : MonoBehaviour
 
   private void FixedUpdate()
   {
-    bool isAcceleratingSameDirectionAsMovement = false;
-    float sidewaysAccelerationModifier = 1f;
-    sidewaysAccelerationModifier *= groundDetection.IsGrounded ? 1f : OFF_GROUND_MODIFIER;
-    float xVelocity = rigidbody2d.velocity.x;
+    ProcessSidewaysAcceleration();
+    ProcessComingToRest();
+  }
 
+  private void ProcessComingToRest()
+  {
+    bool isMovingAndAcceleratingToTheLeft = Input.GetKey(KeyCode.A) && rigidbody2d.velocity.x < 0;
+    bool isMovingAndAcceleratingToTheRight = Input.GetKey(KeyCode.D) && 0 < rigidbody2d.velocity.x;
+    bool isMovingAndAcceleratingInSameDirection = isMovingAndAcceleratingToTheLeft || isMovingAndAcceleratingToTheRight;
+
+    if (isMovingAndAcceleratingInSameDirection)
+      return;
+    // The player is not accelerating in the same direction as they are moving.
+
+    if (!groundDetection.IsGrounded)
+      return;
+    // The player is on the ground.
+
+    // The player should come to rest.
+
+    float newXVelocity =
+      MathF.Sign(rigidbody2d.velocity.x)
+      * MathF.Max(0, MathF.Abs(rigidbody2d.velocity.x) - ACCELERATION * Time.deltaTime);
+    rigidbody2d.velocity = new Vector2(newXVelocity, rigidbody2d.velocity.y);
+  }
+
+  private void ProcessSidewaysAcceleration()
+  {
+    float sidewaysAccelerationDirection = 0f;
     if (Input.GetKey(KeyCode.A))
     {
-      xVelocity = MathF.Max(-MAX_SPEED, xVelocity - sidewaysAccelerationModifier * ACCELERATION * Time.deltaTime);
-      isAcceleratingSameDirectionAsMovement = xVelocity < 0;
+      sidewaysAccelerationDirection -= 1f;
     }
     if (Input.GetKey(KeyCode.D))
     {
-      xVelocity = MathF.Min(MAX_SPEED, xVelocity + sidewaysAccelerationModifier * ACCELERATION * Time.deltaTime);
-      isAcceleratingSameDirectionAsMovement = 0 < xVelocity;
+      sidewaysAccelerationDirection += 1f;
     }
 
-    if (!isAcceleratingSameDirectionAsMovement && groundDetection.IsGrounded)
-    {
-      xVelocity = MathF.Sign(xVelocity) * MathF.Max(0, MathF.Abs(xVelocity) - ACCELERATION * Time.deltaTime);
-    }
+    float sidewaysAccelerationModifier = 1f;
+    sidewaysAccelerationModifier *= groundDetection.IsGrounded ? 1f : OFF_GROUND_MODIFIER;
 
-    rigidbody2d.velocity = new Vector2(xVelocity, rigidbody2d.velocity.y);
+    rigidbody2d.velocity = new Vector2(
+      MathF.Max(
+        -MAX_SPEED,
+        rigidbody2d.velocity.x
+          + sidewaysAccelerationDirection * sidewaysAccelerationModifier * ACCELERATION * Time.deltaTime
+      ),
+      rigidbody2d.velocity.y
+    );
   }
 
   private void Start()
